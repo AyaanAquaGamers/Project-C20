8d1e1127095029ebcbaaf51bd33f41e38bf80f55
AyaanAquaGamers <72394681+AyaanAquaGamers@users.noreply.github.com>
2021-03-05 16:18:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,41 @@
-var bullet,speed,weight
-var wall,thickness
+var car,wall
+var speed,weight
 
 function setup(){
   createCanvas(1600,400)
     
-    speed=random(223,321)
-    weight=random(30,52)
-    thickness=random(22,83)
+    speed=random(55,90)
+    weight=random(400,1500)
 
- bullet=createSprite(50,200,20,10)
- bullet.velocityX=speed
- bullet.shapeColor=color(255)
+ car=createSprite(50,200,50,50)
+ car.velocityX=speed
+ car.shapeColor=color(255)
 
- wall=createSprite(1200,200,thickness,height/2) 
+ wall=createSprite(1370,200,60,height/2) 
  wall.shapeColor=color(80,80,80)
 }
 
 function draw(){
   background("black")
- 
-if (hasCollided(bullet,wall))
-{
- bullet.velocityX=0
- var damage=0.5*weight*speed*speed/(thickness*thickness*thickness)
-
- if(damage>10)
- {
-   wall.shapeColor=color(255,0,0)  
- }
-  
- if(damage<10)
- {
-   wall.shapeColor=color(0,255,0)  
- }
-
-}
-
-
-  drawSprites() 
-} 
-
-
-function hasCollided(lbullet,lwall){
-  bulletRightEdge=lbullet.x+bullet.width
-  wallLeftEdge=lwall.x
-  if (bulletRightEdge>=wallLeftEdge)
+  if (wall.x-car.x<(car.width+wall.width)/2)
   {
-    return true
+    car.velocityX=0
+    var deformation=0.5*weight*speed*speed/22509
+    if(deformation=>180)
+    {
+      car.shapeColor=color(255,0,0)
+    }
+    
+    if(deformation<180 && deformation>80)
+    {
+      car.shapeColor=color(230,230,0)
+    }
+    if(deformation<=80)
+    {
+      car.shapeColor=color(0,255,0)
+    }
   }
-  return false
+
+  console.log(deformation)
+  drawSprites() 
 }
